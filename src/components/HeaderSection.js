9a4f2c5adbd5b3f65ea4fddc617886f8bf10cc29
xkasiaaa3 <104648382+xkasiaaa3@users.noreply.github.com>
2022-12-23 17:16:39 +0100

    link signin

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
 
-import logo_png from './logo.png'
+import logo_png from "./logo.png";
 
-const HeaderSection= () => {
-  return (<div className='Header'><img src={logo_png} alt="logo" width="20%"></img>
-    <input type="text" placeholder="Szukaj..." className='SearchBar'/><button className='SearchButton'>Zaloguj</button></div>
+const HeaderSection = () => {
+  return (
+    <div className="Header">
+      <img src={logo_png} alt="logo" width="20%"></img>
+      <input type="text" placeholder="Szukaj..." className="SearchBar" />
+      <Link to="/signin">
+        <button className="SearchButton">Zaloguj</button>
+      </Link>
+    </div>
   );
 };
 
--- src/components/LoginSection.js
+++ src/components/LoginSection.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
 
-import logo_png from './logo.png'
+import logo_png from "./logo.png";
 
-const LoginSection= () => {
-  return (<div className='Login'>
-    <img src={logo_png} alt="logo" width="30%"></img>
-  <text className='Title'>Login</text>
-    <input type="text" placeholder="Wpisz login" className="Data"/>
-    <text className='Title'>Hasło</text>
-    <input type="password" placeholder="Wpisz hasło" className="Data"/>
-    <button className='SearchButton'>Zaloguj</button></div>
+const LoginSection = () => {
+  return (
+    <div className="Login">
+      <img src={logo_png} alt="logo" width="30%" />
+      <text className="Title">Login</text>
+      <input type="text" placeholder="Wpisz login" className="Data" />
+      <text className="Title">Hasło</text>
+      <input type="password" placeholder="Wpisz hasło" className="Data" />
+      <button className="SearchButton">Zaloguj</button>
+    </div>
   );
- 
 };
 
 export default LoginSection;
